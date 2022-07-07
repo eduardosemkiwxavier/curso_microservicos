aa9e2303940a9b7cee1f3ff01f36b039dbf1cc09
Eduardo Semkiw Xavier <eduardosemkiwxavier@gmail.com>
2022-07-06 23:19:17 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/eduardosemkiwxavier/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,7 @@
+package com.eduardosemkiwxavier.hrworker.repositories;
+
+import com.eduardosemkiwxavier.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
