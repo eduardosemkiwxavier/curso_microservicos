6dd58f5c4ab9c4f04dc296fe2ccff7b481bfc536
Eduardo Semkiw Xavier <eduardosemkiwxavier@gmail.com>
2022-07-08 15:00:37 -0300

    Ribbon load balance

--- hr-payroll/src/main/java/com/eduardosemkiwxavier/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/eduardosemkiwxavier/hrpayroll/services/PaymentService.java
@@ -3,7 +3,10 @@ package com.eduardosemkiwxavier.hrpayroll.services;
 import com.eduardosemkiwxavier.hrpayroll.entities.Payment;
 import com.eduardosemkiwxavier.hrpayroll.entities.Worker;
 import com.eduardosemkiwxavier.hrpayroll.feignclients.WorkerFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +16,7 @@ public class PaymentService {
     private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long worker_id, int days){
+
 
         Worker worker = workerFeignClient.findById(worker_id).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
